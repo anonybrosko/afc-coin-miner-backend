0da9a7ff7b59140f694feae214804b5aa7469aed
anonybrosko <anonybrosko@gmail.com>
2026-03-10 10:48:39 -0700

    First Commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+import random
+
+app = Flask(__name__)
+CORS(app)
+balance = 0
+clicks = 0
+
+@app.route("/mine")
+def mine():
+    global balance
+    global clicks
+    reward = random.randint(1, 5)  # 1-5 AFC Coin
+    balance += reward
+    clicks += 1
+    return jsonify({"reward": reward, "balance": balance, "clicks": clicks})
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
