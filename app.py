07abad15f65d14fb79dac1f9dfeeed3616f19e44
anonybrosko <anonybrosko@gmail.com>
2026-03-10 10:58:22 -0700

    Backend

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ def mine():
     return jsonify({"reward": reward, "balance": balance, "clicks": clicks})
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
