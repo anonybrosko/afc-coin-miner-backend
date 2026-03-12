147ecee462f2014548024ff09ea4d96947783eb8
anonybrosko <anonybrosko@gmail.com>
2026-03-11 23:30:00 -0700

    Updated

--- app.py
+++ app.py
@@ -1,17 +1,48 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import random
 import threading
 import os
 
 from telebot import TeleBot
-from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+from telebot.types import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+
+TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+bot = TeleBot(TOKEN)
 
 app = Flask(__name__)
 CORS(app)
 
 balance = 0
 clicks = 0
+
+@app.route("/")
+def index():
+    return "AFC Coin Miner backend is running!"
+
+@app.route(f"/bot/webhook", methods=["POST"])
+def webhook():
+    json_data = request.get_json()
+
+    if "message" in json_data:
+        update = Update.de_json(json_data)
+        bot.process_new_updates([update])
+    else:
+        print("Webhook recieved non-message JSON", json_data)
+
+    return jsonify({"status": "ok"})
+
+@bot.message_handler(commands=['start'])
+def start(msg):
+    keyboard = InlineKeyboardMarkup()
+    keyboard.add(
+        InlineKeyboardButton(
+            text="Go to Mine 🪙",
+            web_app=WebAppInfo(url="https://anonybrosko.github.io/afc-coin-miner-frontend")
+        )
+    )
+
+    bot.send_message(msg.chat.id, "Welcome! Click below to mine!", reply_markup=keyboard)
 
 @app.route("/mine")
 def mine():
@@ -28,41 +59,12 @@ def mine():
         "clicks": clicks
     })
 
-# -----------------
-# Telegram Bot
-# -----------------
-TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
-
-bot = TeleBot(TOKEN)
-
-@bot.message_handler(commands=['start'])
-def start(msg):
-    keyboard = InlineKeyboardMarkup()
-    keyboard.add(
-        InlineKeyboardButton(
-            text="Go to Mine 🪙",
-            web_app=WebAppInfo(url="https://anonybrosko.github.io/afc-coin-miner-frontend")
-        )
-    )
-
-    bot.send_message(
-        msg.chat.id,
-        "Welcome! Click below to mine:",
-        reply_markup=keyboard
-    )
-
-
-# Run bot in background thread
-def run_bot():
-    print("Telegram bot started")
-    bot.infinity_polling()
-
-
-# -----------------
 # Start both
 # -----------------
 if __name__ == "__main__":
-    threading.Thread(target=run_bot, daemon=True).start()
+    HEROKU_URL = os.environ.get("BACKEND_URL")
+    bot.remove_webhook()
+    bot.set_webhook(url=f"{HEROKU_URL}/bot/webhook")
 
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
